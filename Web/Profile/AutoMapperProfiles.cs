9d4938b0a2059f23fd65300dad1a303d7c2f34f9
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-22 14:25:35 +0300

    edit dto of manager

--- Web/Profile/AutoMapperProfiles.cs
+++ Web/Profile/AutoMapperProfiles.cs
@@ -28,7 +28,8 @@ namespace iread_school_ms.Web.Profile
 
             //School Member Mapper
             CreateMap<SchoolMember, ManagerDto>().ReverseMap();
-            CreateMap<SchoolManagerDto, SchoolMember>().ReverseMap();
+            CreateMap<SchoolManagerDto, SchoolMember>().ReverseMap()
+            .ForMember(dest => dest.Id, act => act.MapFrom(src => src.MemberId));
         }
     }
 }
