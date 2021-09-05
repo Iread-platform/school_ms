b6e11e8f6957ec8e11e213727e2bd4aff3b19505
yazan.codavia <yazankassam.codavia@gmail.com>
2021-09-05 12:51:58 +0300

    include students' class

--- DataAccess/Interface/IClassMemberRepository.cs
+++ DataAccess/Interface/IClassMemberRepository.cs
@@ -7,9 +7,9 @@ namespace iread_school_ms.DataAccess.Interface
     public interface IClassMemberRepository
     {
         public Task<ClassMember> GetById(int id);
-
+        public List<Class> GetByStudent(string studentId);
+        public List<Class> GetByTeacher(string teacherId);
         public void Insert(ClassMember classMember);
-
         public void Delete(ClassMember classMember);
 
     }
