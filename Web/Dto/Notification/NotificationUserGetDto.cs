40098f0938decc044c2d461db5adbdfe31a536ca
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-10-08 21:48:47 +0300

    IR-219

---
+++ Web/Dto/Notification/NotificationUserGetDto.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace iread_school_ms.Web.Dto.Notification
+{
+    public class NotificationUserGetDto
+    {
+        public string UserId { get; set; }
+
+        public string Token
+        {
+            get; set;
+        }
+    }
+}
